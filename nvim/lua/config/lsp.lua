3762fe27681ec567d6a8e8b1b5e130dfe73384a9
wulaladamowang <317670404@qq.com>
2025-11-30 22:59:12 +0800

    patch

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = event.buf, desc = "LSP: Goto Definition" })
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = event.buf, desc = "LSP: Goto Declaration" })
-        vim.keymap.set('n', 'gl', vim.lsp.buf.references, { buffer = event.buf, desc = "LSP: List References" })
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = event.buf, desc = "LSP: List References" })
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = event.buf, desc = "LSP: Desc" })
     end,
 })
